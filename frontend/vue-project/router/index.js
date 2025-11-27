321187ba43839b54da4a8414ef9fcfdb017233a6
luisyepez11 <luisrobert11660@gmail.com>
2025-11-26 22:15:20 -0400

    estrutura front

---
+++ frontend/vue-project/router/index.js
@@ -0,0 +1,20 @@
+// src/router/index.js
+
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/login.vue";
+
+const routes = [
+    {
+        path: '/',
+        name: 'login',
+        component: Login
+    }
+    
+];
+
+const router = createRouter({
+    history: createWebHistory(), 
+    routes
+});
+
+export default router;
